9dd7018f37dfdcd2b03505c63beb43eae22d2304
jacobsfletch <jacobsfletch@gmail.com>
2017-03-14 10:45:00 -0400

    Revise email templates and micropages.

--- app/routes/api/email.js
+++ app/routes/api/email.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var Email = require('keystone-email');
 var keystone = require('keystone');
 
@@ -5,27 +7,64 @@ exports.send = function(req, res) {
     var locals = res.locals,
     data = (req.method == 'POST') ? req.body : req.query;
 
-    new Email('templates/emails/contact.pug', {
-        transport: 'mailgun',
-    }).send({
-        first: data.first,
-        last: data.last,
-        email: data.email,
-        message: data.message
-    }, {
-        apiKey: 'key-e35636e96ae137f973dabfb54cd00f75',
-        domain: 'sandboxc416772e6d3245a58f11a7661725766e.mailgun.org',
-        to: 'Jake <jacobsfletch@gmail.com>',
-        from: 'contact@jacobsfletch.com',
-        subject: 'Contact from jacobsfletch.com',
-    }, function (err, result) {
+    keystone.list('User').model.findOne({
+        key: 'jacob-fletcher'
+    }).exec(function (err, result) {
         if (err) {
-            console.error('Mailgun test failed with error:\n', err);
-        } else {
-            console.log('Successfully sent Mailgun test with result:\n', result);
-            success();
+            console.log(err);
+        }
+        if (result) {
+            contact(result.email);
         }
     });
+
+    function contact(email) {
+        new Email('templates/emails/contact.pug', {
+            transport: 'mailgun',
+        }).send({
+            first: data.first,
+            last: data.last,
+            email: data.email,
+            message: data.message
+        }, {
+            apiKey: process.env.MAILGUN_API_KEY,
+            domain: process.env.MAILGUN_DOMAIN,
+            to: email,
+            from: 'jacobsfletch.com <contact@jacobsfletch.com>',
+            subject: 'Contact from jacobsfletch.com',
+        }, function (err, result) {
+            if (err) {
+                console.error('Mailgun test failed with error:\n', err);
+            } else {
+                console.log('Successfully sent Mailgun test with result:\n', result);
+                confirm();
+            }
+        });
+    }
+
+    function confirm() {
+        new Email('templates/emails/confirmation.pug', {
+            transport: 'mailgun',
+        }).send({
+            first: data.first,
+            last: data.last,
+            email: data.email,
+            message: data.message
+        }, {
+            apiKey: process.env.MAILGUN_API_KEY,
+            domain: process.env.MAILGUN_DOMAIN,
+            to: data.email,
+            from: 'jacobsfletch.com <confirmation@jacobsfletch.com>',
+            subject: 'Confirmation from jacobsfletch.com',
+        }, function (err, result) {
+            if (err) {
+                console.error('Mailgun test failed with error:\n', err);
+            } else {
+                console.log('Successfully sent Mailgun test with result:\n', result);
+                success();
+            }
+        });
+    }
 
     function success() {
         res.redirect('/contact/confirmation')
