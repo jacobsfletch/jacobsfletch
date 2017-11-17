a06ce20ce2d35f5d5f206550a08a59011b027813
Jacob Fletcher <jacobsfletch@gmail.com>
2017-11-17 15:47:05 -0500

    Create breadcrumbs components in footer.

--- app/src/components/layouts/footer/footer.js
+++ app/src/components/layouts/footer/footer.js
@@ -2,19 +2,30 @@ import React from 'react'
 import Timer from '../../modules/timer/Timer'
 import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 import './footer.css'
 
 class Footer extends React.Component {
     render() {
         const path = this.props.location.pathname.split("/")
-        path.splice(0, 1, "index")
+        path[0] = "index"
+        let links = path.map(function(link) {
+            return (
+                <li className={'list-item'}>
+                    <Link to={`/${link}`}>
+                        {link}
+                    </Link>
+                </li>
+            )
+        })
+        //path.splice(0, 1, "index")
         return (
             <div className="app-footer">
                 <div className="cropmark bl"></div>
-                <div className="footer-breadcrumbs">
-                    {path}
-                </div>
+                <ul className="footer-breadcrumbs">
+                    {links}
+                </ul>
                 <div className="footer-title">
                     <Timer />
                 </div>
